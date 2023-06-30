1c843abfabfeee942ab841338e3cca053207ef05
fitz35 <clement@lahoche.info>
2023-06-30 14:45:29 +0200

    add custom dialog window

--- src/gui/MainFrame.py
+++ src/gui/MainFrame.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from gui.NewVideoFrame import NewVideoFrame
 
 from utils.utils import validate_youtube_link
 
@@ -9,15 +10,19 @@ class MainFrame:
 
     def create_widgets(self) -> None:
         self.label = tk.Label(self.root, text="Video link :")
-        self.label.pack()
+        self.label.grid(row=0, column=0, columnspan = 2)
 
         # Create an Entry widget
         self.yt_link_entry = tk.Entry(self.root, width=50)
-        self.yt_link_entry.pack()
+        self.yt_link_entry.grid(row=1, column=0, padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text="Click Me", command=self.button_clicked)
-        self.button.pack()
+        self.button = tk.Button(self.root, text="Validez !", command=self.button_clicked)
+        self.button.grid(row=1, column=1, padx=10, pady=10)
 
     def button_clicked(self) -> None:
-        print("Button clicked")
         print(validate_youtube_link(self.yt_link_entry.get()))
+        # open the new video frame and attache the callback
+        self.diag = NewVideoFrame(self.root, lambda : self.newVideoClosed())
+
+    def newVideoClosed(self):
+        print("close new video closed")
---
+++ src/gui/NewVideoFrame.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from typing import Callable
+
+class NewVideoFrame:
+    """
+    add a video to the list
+    """
+    def __init__(self, parent: tk.Tk, onFinish : Callable[[], None]) -> None:
+        """
+        create a dialog window to add video to the list
+        @params parent : the tk root
+        @params onFinish : callback on finish
+        """
+        self.parent = parent
+        self.onFinish = onFinish
+
+        self.dialog = tk.Toplevel(parent)
+        self.dialog.title("Custom Dialog")
+        self.dialog.protocol("WM_DELETE_WINDOW", self.on_close)
+
+        self.__create_widgets()
+
+    def __create_widgets(self) -> None:
+        label = tk.Label(self.dialog, text="Enter your name:")
+        label.pack()
+
+        self.entry = tk.Entry(self.dialog)
+        self.entry.pack()
+
+        button = tk.Button(self.dialog, text="Submit", command=self.__submit_dialog)
+        button.pack()
+
+    def __submit_dialog(self) -> None:
+        name = self.entry.get()
+        self.onFinish()
+        self.dialog.destroy()
+
+    def on_close(self) -> None:
+        self.dialog.destroy()
